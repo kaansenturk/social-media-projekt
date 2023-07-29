88184ac5a33eb0d050d886b96d8c2b11bb9e00a9
Johann Beaumont <johann.beaumont@hotmail.de>
2023-07-29 04:04:01 +0200

    Datenbankframework aufgesetzt

--- src/app.py
+++ src/app.py
@@ -1,14 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Depends
 from fastapi.middleware.cors import CORSMiddleware
 import logging
 from starlette.responses import RedirectResponse
 import sqlite3
+
+from sqlalchemy.orm import Session
+
+import crud, models, schemas
+from database import SessionLocal, engine
 
 logging.basicConfig(filename='../logs/api.log', encoding='utf-8',format='%(asctime)s %(levelname)-8s %(message)s',datefmt='%Y-%m-%d %H:%M:%S', level=logging.DEBUG)
 
 logger=logging.getLogger("fast_api_logger")
 
 logger.setLevel(logging.INFO)
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
@@ -24,6 +31,49 @@ app.add_middleware(
 
 DBNAME = 'social_media.db'
 
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.post("/users/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return crud.create_user(db=db, user=user)
+
+
+@app.get("/users/", response_model=list[schemas.User])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = crud.get_users(db, skip=skip, limit=limit)
+    return users
+
+
+@app.get("/users/{user_id}", response_model=schemas.User)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
+
+@app.post("/users/{user_id}/items/", response_model=schemas.Item)
+def create_item_for_user(
+    user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
+):
+    return crud.create_user_item(db=db, item=item, user_id=user_id)
+
+
+@app.get("/items/", response_model=list[schemas.Item])
+def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    items = crud.get_items(db, skip=skip, limit=limit)
+    return items
+
 @app.get("/")
 async def root():
     return RedirectResponse(url="/docs")
@@ -31,4 +81,43 @@ async def root():
 @app.get("/user_data")
 def get_all_users():
     conn = sqlite3.connect(DBNAME)
-    cur = conn.cursor()
+    c = conn.cursor()
+
+    c.execute('''CREATE TABLE IF NOT EXISTS testTable (name TEXT, number IINTEGER)''')
+    c.execute('''INSERT INTO testTable (name, number) VALUES ("John", 3)''')
+    c.execute('''SELECT * FROM testTable;''')
+    results = c.fetchall()
+
+    print(results)
+    
+    conn.commit()
+    conn.close()
+
+    return results   
+
+@app.post("/dbTest")
+def testDB():
+    conn = sqlite3.connect(DBNAME)
+    c = conn.cursor()
+
+    c.execute('''CREATE TABLE IF NOT EXISTS testTable (name TEXT, number IINTEGER)''')
+    c.execute('''INSERT INTO testTable (name, number) VALUES ("John", 3)''')
+    c.execute('''SELECT * FROM testTable;''')
+    results = c.fetchall()
+
+    print(results)
+    
+    conn.commit()
+    conn.close()
+
+    return results
+
+@app.post("/createUser")
+def createUser(first_name, last_name , email, birthday, username):
+    conn = sqlite3.connect(DBNAME)
+    c = conn.cursor()
+
+    c.execute('''INSERT INTO users (first_name, last_name, email, birthday, username) VALUES ()''')
+
+    conn.commit()
+    conn.close()
